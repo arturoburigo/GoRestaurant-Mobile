b5afa6b0119e5f39cfe6f746078dfd179f0fb741
Arturo Burigo <burigoarturo3@gmail.com>
2020-08-24 17:49:22 -0300

    Add files via upload

---
+++ src/pages/FoodDetails/index.tsx
@@ -0,0 +1,277 @@
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useMemo,
+  useLayoutEffect,
+} from 'react';
+import { Image } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import formatValue from '../../utils/formatValue';
+
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  ScrollContainer,
+  FoodsContainer,
+  Food,
+  FoodImageContainer,
+  FoodContent,
+  FoodTitle,
+  FoodDescription,
+  FoodPricing,
+  AdditionalsContainer,
+  Title,
+  TotalContainer,
+  AdittionalItem,
+  AdittionalItemText,
+  AdittionalQuantity,
+  PriceButtonContainer,
+  TotalPrice,
+  QuantityContainer,
+  FinishOrderButton,
+  ButtonText,
+  IconContainer,
+} from './styles';
+
+interface Params {
+  id: number;
+}
+
+interface Extra {
+  id: number;
+  name: string;
+  value: number;
+  quantity: number;
+}
+
+interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  image_url: string;
+  formattedPrice: string;
+  extras: Extra[];
+}
+
+const FoodDetails: React.FC = () => {
+  const [food, setFood] = useState({} as Food);
+  const [extras, setExtras] = useState<Extra[]>([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [foodQuantity, setFoodQuantity] = useState(1);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    async function loadFood(): Promise<void> {
+      const response = await api.get<Food>(`/foods/${routeParams.id}`);
+
+      const foodDetails = response.data;
+
+      const serializedFood = {
+        ...foodDetails,
+        formattedPrice: formatValue(foodDetails.price),
+      };
+
+      setFood(serializedFood);
+
+      const serializedExtras = serializedFood.extras.map(extra => {
+        return {
+          ...extra,
+          quantity: 0,
+        };
+      });
+      setExtras(serializedExtras);
+    }
+
+    loadFood();
+  }, [routeParams]);
+
+  function handleIncrementExtra(id: number): void {
+    const newExtras = [...extras];
+    const findExtra = newExtras.find(extra => extra.id === id);
+
+    if (findExtra) {
+      findExtra.quantity += 1;
+
+      setExtras(newExtras);
+    }
+  }
+
+  function handleDecrementExtra(id: number): void {
+    const newExtras = [...extras];
+    const findExtra = newExtras.find(extra => extra.id === id);
+
+    if (findExtra) {
+      if (findExtra.quantity > 0) {
+        findExtra.quantity -= 1;
+      }
+
+      setExtras(newExtras);
+    }
+  }
+
+  function handleIncrementFood(): void {
+    setFoodQuantity(foodQuantity + 1);
+  }
+
+  function handleDecrementFood(): void {
+    if (foodQuantity > 1) {
+      setFoodQuantity(foodQuantity - 1);
+    }
+  }
+
+  const toggleFavorite = useCallback(() => {
+    setIsFavorite(!isFavorite);
+  }, [isFavorite]);
+
+  const cartTotal = useMemo(() => {
+    const total = food.price * foodQuantity;
+
+    return extras.reduce((acc, item) => {
+      if (item.quantity) {
+        return acc + item.quantity * item.value;
+      }
+
+      return acc;
+    }, total);
+  }, [extras, food, foodQuantity]);
+
+  async function handleFinishOrder(): Promise<void> {
+    const {
+      id: product_id,
+      name,
+      description,
+      image_url: thumbnail_url,
+    } = food;
+
+    const order = {
+      product_id,
+      name,
+      description,
+      price: cartTotal,
+      thumbnail_url,
+      extras,
+    };
+
+    await api.post('/orders', order);
+  }
+
+  // Calculate the correct icon name
+  const favoriteIconName = useMemo(
+    () => (isFavorite ? 'favorite' : 'favorite-border'),
+    [isFavorite],
+  );
+
+  useLayoutEffect(() => {
+    // Add the favorite icon on the right of the header bar
+    navigation.setOptions({
+      headerRight: () => (
+        <MaterialIcon
+          name={favoriteIconName}
+          size={24}
+          color="#FFB84D"
+          onPress={() => toggleFavorite()}
+        />
+      ),
+    });
+  }, [navigation, favoriteIconName, toggleFavorite]);
+
+  return (
+    <Container>
+      <Header />
+
+      <ScrollContainer>
+        <FoodsContainer>
+          <Food>
+            <FoodImageContainer>
+              <Image
+                style={{ width: 327, height: 183 }}
+                source={{
+                  uri: food.image_url,
+                }}
+              />
+            </FoodImageContainer>
+            <FoodContent>
+              <FoodTitle>{food.name}</FoodTitle>
+              <FoodDescription>{food.description}</FoodDescription>
+              <FoodPricing>{food.formattedPrice}</FoodPricing>
+            </FoodContent>
+          </Food>
+        </FoodsContainer>
+        <AdditionalsContainer>
+          <Title>Adicionais</Title>
+          {extras.map(extra => (
+            <AdittionalItem key={extra.id}>
+              <AdittionalItemText>{extra.name}</AdittionalItemText>
+              <AdittionalQuantity>
+                <Icon
+                  size={15}
+                  color="#6C6C80"
+                  name="minus"
+                  onPress={() => handleDecrementExtra(extra.id)}
+                  testID={`decrement-extra-${extra.id}`}
+                />
+                <AdittionalItemText testID={`extra-quantity-${extra.id}`}>
+                  {extra.quantity}
+                </AdittionalItemText>
+                <Icon
+                  size={15}
+                  color="#6C6C80"
+                  name="plus"
+                  onPress={() => handleIncrementExtra(extra.id)}
+                  testID={`increment-extra-${extra.id}`}
+                />
+              </AdittionalQuantity>
+            </AdittionalItem>
+          ))}
+        </AdditionalsContainer>
+        <TotalContainer>
+          <Title>Total do pedido</Title>
+          <PriceButtonContainer>
+            <TotalPrice testID="cart-total">
+              {formatValue(cartTotal)}
+            </TotalPrice>
+            <QuantityContainer>
+              <Icon
+                size={15}
+                color="#6C6C80"
+                name="minus"
+                onPress={handleDecrementFood}
+                testID="decrement-food"
+              />
+              <AdittionalItemText testID="food-quantity">
+                {foodQuantity}
+              </AdittionalItemText>
+              <Icon
+                size={15}
+                color="#6C6C80"
+                name="plus"
+                onPress={handleIncrementFood}
+                testID="increment-food"
+              />
+            </QuantityContainer>
+          </PriceButtonContainer>
+
+          <FinishOrderButton onPress={() => handleFinishOrder()}>
+            <ButtonText>Confirmar pedido</ButtonText>
+            <IconContainer>
+              <Icon name="check-square" size={24} color="#fff" />
+            </IconContainer>
+          </FinishOrderButton>
+        </TotalContainer>
+      </ScrollContainer>
+    </Container>
+  );
+};
+
+export default FoodDetails;
